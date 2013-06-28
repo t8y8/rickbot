390fb923712c0bbe04d8b7e0b7031dbb3c084af6
Tyler Doyle <kingt8y8@gmail.com>
2013-06-28 09:56:53 -0700

    Added logging
    Cleanup for dev code, added logging to most routes.

--- app/rick.py
+++ app/rick.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import logging
 import sqlite3
 import random
 import sys
@@ -10,17 +11,19 @@ version = "3.5"
 #Constants
 DB_FILE = 'rick.db'
 
-try:
-    if sys.argv[1].lower() == 'debug':
-        PORT = 8080
-        SERVER = 'wsgiref'
-        HOST = '127.0.0.1'
-except:
+if len(sys.argv) > 0 and sys.argv[1].lower() == 'debug':
+    PORT = 8080
+    SERVER = 'wsgiref'
+    HOST = '127.0.0.1'
+else:
     PORT = 80
     SERVER = 'cherrypy'
     HOST = '0.0.0.0'
 
+#Logging
+logging.basicConfig(filename="rickbot.log", level=logging.INFO)
 
+#Create the explicit application object
 app = Bottle()
 
 
@@ -28,11 +31,14 @@ app = Bottle()
 def get_quote_from_db(id_no=None):
     '''Retreive a random saying from the DB'''
     if not id_no:
+        logging.info("Querying DB for IDs")
         idx_lst = query_db("SELECT id FROM sayings", DB_FILE)
         idx = random.choice(idx_lst)[0]  # extracts a random id
     else:
         idx = id_no
-    result = query_db("SELECT saying FROM sayings WHERE id = ?", DB_FILE, params=(idx,))[0]
+    logging.info("Querying DB for specific quote {}".format(id_no))
+    result = query_db("SELECT saying FROM sayings WHERE id = ?",
+                      DB_FILE, params=(idx,))[0]
     # return the saying AND index so we can generate the static link
     return (result[0].encode("8859", "ignore").decode("utf8","ignore"), idx)
 
@@ -41,7 +47,9 @@ def insert_quote_into_db(text):
     '''Insert Rick's saying into the DB'''
     now_date = str(datetime.now().replace(microsecond=0))  # No microseconds
     val_text = clean_text(text)
-    insert_db("INSERT INTO sayings (date, saying) VALUES (?,?)", (now_date, val_text), DB_FILE)
+    logging.info("INSERTING {} into DB".format())
+    insert_db("INSERT INTO sayings (date, saying) VALUES (?,?)",
+              (now_date, val_text), DB_FILE)
 
 
 def alpha_only(text):
@@ -50,12 +58,15 @@ def alpha_only(text):
 
 def check_no_dupe(text):
     dupes = []
+    logging.info("Querying DB to check for duplicates")
     results = query_db("SELECT id, saying FROM sayings", DB_FILE)
+    logging.info("Checking for already existing quote: {}".format(text))
     for row in results:
         quote = alpha_only(row[1])
         dupes.append(hash(quote))
     inst_text = alpha_only(text)
     if hash(inst_text) in dupes:
+        logging.warning("Quote '{}' is a duplicate".format(text))
         return False
     else:
         return True
@@ -64,8 +75,10 @@ def query_db(query, db, *, params=None):
     with sqlite3.connect(db) as db:
         cur = db.cursor()
         if params:
+            logging.info("Sending Query: {} with {}".format(query, params))
             res = cur.execute(query, params).fetchall()
         else:
+            logging.info("Sending Query: {}".format(query))
             res = cur.execute(query).fetchall()
         return res
 
@@ -73,6 +86,7 @@ def insert_db(query, vals, db):
     with sqlite3.connect(db) as db:
         cur = db.cursor()
         try:
+            logging.info("Inserting: {} --> {}".format(query, vals))
             res = cur.execute(query, vals)
             db.commit()
         except Exception as e:
@@ -118,6 +132,7 @@ def get_favicon():
 @app.route('/')
 def index():
     '''Returns the index page with a randomly chosen RickQuote'''
+    logging.info("{} requested a quote".format(request.remote_addr))
     quote_and_saying = get_quote_from_db()
     rick_quote = quote_and_saying[0]
     quote_no = quote_and_saying[1]
@@ -135,6 +150,7 @@ def redirect_to_index():
 @app.route('/quote', method="POST")
 def put_quote():
     '''route for submitting quote'''
+    logging.info("{} is submitting a quote".format(request.remote_addr))
     unval_quote = request.forms.get('saying')
     if len(unval_quote) > 4 and check_no_dupe(unval_quote):  # arbitrary len
         insert_quote_into_db(unval_quote)
